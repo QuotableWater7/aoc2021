b4b3f265da34839deb9d19578326a9731a8b70c5
JT <jt@usekojo.com>
2023-01-14 23:35:18 -0800

    part 2 complete

--- 02/main.hs
+++ 02/main.hs
@@ -1,14 +1,13 @@
 import qualified Data.List.Split as S
 import Text.RE.Summa (parseInteger)
 
-data Direction = Up | Down | Forward | Backward deriving(Show)
+data Direction = Up | Down | Forward deriving(Show)
 
 parseDirection :: String -> Either String Direction
 parseDirection str
   | str == "up"        = return Up
   | str == "down"      = return Down
   | str == "forward"   = return Forward
-  | str == "backward"  = return Backward
   | otherwise          = Left $ "Could not parse: " ++ str
 
 data Move = Move Direction Int deriving(Show)
@@ -25,10 +24,16 @@ parseMove str = do
 calculatePosition :: [Move] -> (Int, Int) 
 calculatePosition = foldr calculatePositionHelper (0, 0)
   where 
-    calculatePositionHelper (Move Down n) (x, z) = (x, z + n)
-    calculatePositionHelper (Move Up n) (x, z) = (x, z - n)
-    calculatePositionHelper (Move Forward n) (x, z) = (x + n, z)
-    calculatePositionHelper (Move Backward n) (x, z) = (x - n, z)
+    calculatePositionHelper (Move Down n) (x, y) = (x, y + n)
+    calculatePositionHelper (Move Up n) (x, y) = (x, y - n)
+    calculatePositionHelper (Move Forward n) (x, y) = (x + n, y)
+
+calculatePositionPart2 :: [Move] -> (Int, Int, Int)
+calculatePositionPart2 = foldl calculatePositionHelper2 (0, 0, 0)
+  where
+    calculatePositionHelper2 (x, y, z) (Move Down n) = (x, y, z + n)
+    calculatePositionHelper2 (x, y, z) (Move Up n) = (x, y, z - n)
+    calculatePositionHelper2 (x, y, z) (Move Forward n) = (x + n, y + z * n, z)
 
 main = do
   contents <- readFile "input.txt"
@@ -38,4 +43,10 @@ main = do
   let position = calculatePosition <$> moves
   case position of
     Right (x, z) -> print $ "(" ++ show x ++ ", " ++ show z ++ ") = " ++ show (x * z) 
+    Left reason -> print $ "Could not calculate position: " ++ reason
+
+  -- part 2
+  let position = calculatePositionPart2 <$> moves
+  case position of
+    Right (x, y, _) -> print $ "(" ++ show x ++ ", " ++ show y ++ ") = " ++ show (x * y) 
     Left reason -> print $ "Could not calculate position: " ++ reason
